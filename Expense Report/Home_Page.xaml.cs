6e9154fee866db6d0ab7c3608e5ff68c53883ed2
mohamed moustafa fathy <mohamed fathy@ELSEWEDY-SCHOOL.LOCAL>
2026-02-16 15:08:02 +0200

    Initial commit

---
+++ Expense Report/Home_Page.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expense_Report
+{
+    /// <summary>
+    /// Interaction logic for Home_Page.xaml
+    /// </summary>
+    public partial class Home_Page : Page
+    {
+        public Home_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Select_button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Report());
+        }
+    }
+}
---
+++ Expense Report/LogIn.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expense_Report
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Page
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInbtn_Click(object sender, RoutedEventArgs e)
+        {
+            var userName = UserName.Text;
+            var password = Password.Password;
+
+            if(userName == "mohamed" && password == "MF_RTX")
+            {
+                NavigationService.Navigate(new Home_Page());
+                return;
+            }
+
+            MessageBox.Show("Username or password is worng", "Error", MessageBoxButton.OK);
+        }
+    }
+}
